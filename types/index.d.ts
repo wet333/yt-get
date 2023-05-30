d22bc4a7e66b2c536326f35f8b9a1e43b8a3728c
Agustin Wet <wet.4gustin@gmail.com>
2023-05-29 22:15:55 -0300

    new types for new api

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,7 @@
-declare module 'yt-get' {
-    function downloadPlaylist(playlistURL: string): Promise<void>;
+declare module "yt-get" {
+    function getVideoMP3Base64(videoURL: string): Promise<{ base64: string, title: string }>;
+    function getVideoMP3Binary(videoURL: string): Promise<{ mp3: Buffer, title: string }>;
     function downloadVideo(videoURL: string): Promise<void>;
-    function downloadVideoAsBase64(videoURL: string): Promise<string>;
+    function downloadPlaylist(playlistURL: string): Promise<void>;
+    function getVideoTitle(videoURL: string): string;
 }
--- types/utils.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'utils' {
-    function sanitizeFilename(filename: string): string;
-    function createOutputFolder(folderName: string): Promise<void>;
-}
